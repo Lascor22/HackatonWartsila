7a21f4d3f5fc2015042fe06b098c1cda7397078b
dmozh99 <dmozh99@mail.ru>
2020-02-15 18:30:57 +0300

    initial commit

---
+++ officemaps/src/main/java/com/blagoy/officemaps/OfficemapsApplication.java
@@ -0,0 +1,13 @@
+package com.blagoy.officemaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficemapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficemapsApplication.class, args);
+	}
+
+}
